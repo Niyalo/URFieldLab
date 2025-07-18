ff1bf03d7bdfe5cdb2cd147f5ccc6ed72bcd5bc5
Kushal Shrestha <kushal-shrestha@hotmail.com>
2025-07-18 17:38:52 +0545

    setup netlify

--- urfield-lab/app/page.tsx
+++ urfield-lab/app/page.tsx
@@ -12,6 +12,12 @@ export default function Home() {
           >
             Content Management
           </Link>
+          <Link
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="/about"
+          >
+            About
+          </Link>
         </div>
       </main>
     </div>
--- urfield-lab/next.config.ts
+++ urfield-lab/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
+  images: {
     domains: ['cdn.sanity.io'],
   },
   /* config options here */
